16560bf2fbb72d2874eefb980d4bdccdfda9b3fc
morikentarou <routatarou1119@gmail.com>
2024-07-29 22:51:21 +0900

    ユーザー機能カテゴリー実装

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,15 +1,67 @@
 
 
-private
+class GoalsController < ApplicationController
 
-def goal_params
-  params.require(:goal).permit(:goal_kcal, :goal_oil, :goal_protein, :goal_sugar, :pfctype_id).merge(user_id: current_user.id)
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @goals = current_user.goals.order("created_at DESC")
+    @goals = @goals.where('name LIKE ?', "%#{params[:keyword]}%") if params[:keyword].present?
+    @goals = @goals.where(pfctype_id: params[:pfctype_id]) if params[:pfctype_id].present?
+    Rails.logger.debug "Goals: #{@goals.inspect}"
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to goals_path
+    else 
+      render :new
+    end
+  end
+
+  def destroy
+    goal = Goal.find(params[:id])
+    goal.destroy
+    redirect_to goals_path
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    goal = Goal.find(params[:id])
+    goal.update(goal_params)
+    redirect_to goals_path
+  end
+
+  def search
+    @goals = current_user.goals.order("created_at DESC")
+    @goals = @goals.where('name LIKE ?', "%#{params[:keyword]}%") if params[:keyword].present?
+    @goals = @goals.where(pfctype_id: params[:pfctype_id]) if params[:pfctype_id].present?
+    render :index
+  end
+
+  def select
+    session[:selected_goal_id] = params[:id]
+    redirect_to pfcs_path
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:goal_kcal, :goal_oil, :goal_protein, :goal_sugar, :pfctype_id).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @goal = Goal.find(params[:id])
+    redirect_to(root_url) unless current_user == @goal.user
+  end
+
 end
-
-def correct_user
-  @goal = Goal.find(params[:id])
-  redirect_to(root_url) unless current_user == @goal.user
-end
-
-end
-
--- app/controllers/pfcs_controller.rb
+++ app/controllers/pfcs_controller.rb
@@ -1,6 +1,17 @@
 class PfcsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   
   def index
+    @pfc = Pfc.all
+    @goal = Goal.find(session[:selected_goal_id]) if session[:selected_goal_id].present?
+  end
+
+  private
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
