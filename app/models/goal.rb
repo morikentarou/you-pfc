aeca699c27746a6eb704be805b3ce88996386193
morikentarou <routatarou1119@gmail.com>
2024-07-30 01:23:46 +0900

    目標一覧グラフの実装

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -7,5 +7,21 @@ class Goal < ApplicationRecord
   validates :goal_oil, presence: true
   validates :goal_protein, presence: true
   validates :goal_sugar, presence: true
+  
+  def protein_percentage
+    (goal_protein.to_f * 4 / total_calories * 100).floor
+  end
+
+  def sugar_percentage
+    (goal_sugar.to_f * 4 / total_calories * 100).floor
+  end
+
+  def oil_percentage
+    (goal_oil.to_f * 9 / total_calories * 100).floor
+  end
+
+  def total_calories
+    (goal_protein * 4) + (goal_sugar * 4) + (goal_oil * 9)
+  end
 
 end
