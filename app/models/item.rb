081454d932cbe6a47818d4c71e1c8221f73aadb4
morikentarou <routatarou1119@gmail.com>
2024-07-30 13:59:35 +0900

    バリエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
 
   belongs_to :user
   
+  validates :item_name, presence: true
+  validates :item_kcal, presence: true
+  validates :item_oil, presence: true
+  validates :item_protein, presence: true
+  validates :item_sugar, presence: true
 end
--- app/models/pfc.rb
+++ app/models/pfc.rb
@@ -5,4 +5,8 @@ class Pfc < ApplicationRecord
   has_many :items, through: :items_pfcs
 
   belongs_to :user
+
+  validates :day,                 presence: true
+  validates :time,                presence: true
+  validates :timezone_id,         presence: true, numericality: { other_than: 1, message: "can't be blank" }
 end
