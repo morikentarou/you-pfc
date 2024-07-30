0d8428ed5d15e72d45b26b739c98b68543669ba7
morikentarou <routatarou1119@gmail.com>
2024-07-30 12:08:13 +0900

    pfcコントローラー記述

--- app/controllers/pfcs_controller.rb
+++ app/controllers/pfcs_controller.rb
@@ -1,5 +1,6 @@
 class PfcsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!
+  before_action :move_to_index, except: [:index]
   
   def index
     @pfc = Pfc.all
@@ -10,9 +11,40 @@ class PfcsController < ApplicationController
     @pfc = Pfc.new
   end
 
-  
+  def create
+    @pfc = Pfc.new(item_params)
+    if @pfc.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @pfc.update(pfc_params)
+      redirect_to pfc_path(@pfc)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @pfc.destroy
+    redirect_to root_path
+  end
+
 
   private
+
+  def item_params
+    params.require(:pfc).permit(:,).merge(user_id: current_user.id)
+  end
   
   def move_to_index
     unless user_signed_in?
