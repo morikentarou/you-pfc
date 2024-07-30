80d76af8a8616d1703ca7184171644e5991c5753
morikentarou <routatarou1119@gmail.com>
2024-07-30 13:17:35 +0900

    中間テーブルの値編集

--- db/migrate/20240730034348_create_items_pfcs.rb
+++ db/migrate/20240730034348_create_items_pfcs.rb
@@ -1,9 +1,9 @@
 class CreateItemsPfcs < ActiveRecord::Migration[7.0]
   def change
-    create_table :pfcs_items do |t|
+    create_table :items_pfcs do |t|
 
-      t.references :pfc_id,           null: false, foreign_key: true
-      t.references :item_id,          null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.references :pfc, null: false, foreign_key: true
 
       t.timestamps
     end
