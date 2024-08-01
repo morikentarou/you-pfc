868ee80ee2646b716498eb71f182f3efce62414d
morikentarou <routatarou1119@gmail.com>
2024-08-01 15:07:40 +0900

    マイグレーションの順番変更

--- db/migrate/20240729120045_create_pfcs.rb
+++ db/migrate/20240729120045_create_pfcs.rb
@@ -1,12 +1,11 @@
 class CreatePfcs < ActiveRecord::Migration[7.0]
   def change
     create_table :pfcs do |t|
-      t.references :user,        null: false, foreign_key: true
-      t.references :item,        null: false, foreign_key: true
       t.date       :day,         null: false
       t.datetime   :time,        null: false
-
       t.integer    :timezone_id, null: false
+      t.references :user,        null: false, foreign_key: true
+      t.references :item,        null: false, foreign_key: true
 
       t.timestamps
     end
