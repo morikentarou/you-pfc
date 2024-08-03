173b893bf43597433f0dcacdf80d344825430091
morikentarou <routatarou1119@gmail.com>
2024-08-03 17:39:18 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth
+  before_action :basic_auth, only: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show] 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
