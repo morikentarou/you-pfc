32037f94f378b2792e37e1cc3c80ebfb80fafbd6
morikentarou <routatarou1119@gmail.com>
2024-08-17 03:44:34 +0900

    ％の表示

--- app/javascript/new_pfc.js
+++ app/javascript/new_pfc.js
@@ -3,12 +3,7 @@ document.addEventListener('turbo:load', () => {
     document.querySelectorAll('input[name="pfc[item_ids][]"]').forEach(checkbox => {
       const itemId = checkbox.value;
       const percentageField = document.getElementById(`percentage_${itemId}`);
-      
-      if (checkbox.checked) {
-        percentageField.style.display = 'inline-block'; // 表示する
-      } else {
-        percentageField.style.display = 'none'; // 非表示にする
-      }
+      percentageField.style.display = 'inline-block'; // 表示する
     });
   };
 
@@ -20,5 +15,21 @@ document.addEventListener('turbo:load', () => {
     if (event.target.matches('input[name="pfc[item_ids][]"]')) {
       togglePercentageFields();
     }
+  });
+});
+
+// Turbo Frame が更新されたときにチェックボックスのリスナーを再設定
+document.addEventListener('turbo:frame-load', () => {
+  console.log('Turbo frame load event fired');
+  const checkboxes = document.querySelectorAll('input[name="pfc[item_ids][]"]');
+  
+  // チェックボックスの状態に応じて％フィールドを調整
+  togglePercentageFields();
+
+  // チェックボックスの状態が変更されたときに％フィールドを調整
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      togglePercentageFields();
+    });
   });
 });
