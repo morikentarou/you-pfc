719642f2eb955a6b294ae0e1f394e36da516dec2
morikentarou <routatarou1119@gmail.com>
2024-08-19 02:00:46 +0900

    ＰＦＣ削除機能追加

--- app/controllers/pfcs_controller.rb
+++ app/controllers/pfcs_controller.rb
@@ -1,9 +1,10 @@
 class PfcsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_pfc, only: [:edit, :update, :destroy]
   
   def index
-    @pfc = Pfc.all
+    @pfcs = current_user.pfcs.includes(:items)
     if session[:selected_goal_id].present?
       begin
         @goal = Goal.find(session[:selected_goal_id])
@@ -60,23 +61,19 @@ class PfcsController < ApplicationController
   end
 
   def update
-    @pfc = Pfc.new(pfc_params)
+    @pfc = Pfc.find(params[:id])
     @pfc.user = current_user
     @items = current_user.items
     selected_item_ids = params[:pfc][:item_ids] || []
     adjustment_percentages = params[:pfc][:adjustment_percentages] || {}
-
+  
     if selected_item_ids.blank?
       @pfc.errors.add(:item_ids, "を選択してください。")
-      render :new
+      render :edit
       return
     end
-
-    selected_item_ids.each do |item_id|
-      percentage = adjustment_percentages[item_id.to_s] || 100
-    end
-
-    if @pfc.save
+  
+    if @pfc.update(pfc_params)
       adjust_items
       redirect_to edit_pfc_path(@pfc)
     else
@@ -85,8 +82,11 @@ class PfcsController < ApplicationController
   end
 
   def destroy
-    @pfc.destroy
-    redirect_to root_path
+    if @pfc.destroy
+      redirect_to pfcs_path, notice: 'PFCが削除されました。'
+    else
+      redirect_to pfcs_path, alert: 'PFCの削除に失敗しました。'
+    end
   end
 
 
@@ -103,7 +103,7 @@ class PfcsController < ApplicationController
   end
 
   def adjust_items
-    adjustment_percentages = params[:pfc][:adjustment_percentages].transform_values(&:to_f)
+    adjustment_percentages = (params[:pfc][:adjustment_percentages] || {}).transform_values(&:to_f)
     selected_item_ids = params[:pfc][:item_ids].map(&:to_i)
   
     selected_item_ids.each do |item_id|
@@ -119,6 +119,10 @@ class PfcsController < ApplicationController
         adjustment_percentage: adjustment_percentage
       )
     end
+  end
+
+  def set_pfc
+    @pfc = Pfc.find(params[:id])
   end
 
 end
--- app/models/pfc.rb
+++ app/models/pfc.rb
@@ -1,6 +1,6 @@
 class Pfc < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  has_many :pfc_items
+  has_many :pfc_items, dependent: :destroy
   has_many :items, through: :pfc_items
   
   belongs_to :user
@@ -9,4 +9,20 @@ class Pfc < ApplicationRecord
   validates :day,                 presence: { message: "を入力してください。" }
   validates :time,                presence: { message: "を入力してください。" }
   validates :timezone_id,         presence: { message: "を選択してください。" }, numericality: { other_than: 1, message: "can't be blank" }
+
+  def total_kcal
+    pfc_items.sum(:adjusted_kcal)
+  end
+
+  def total_protein
+    pfc_items.sum(:adjusted_protein)
+  end
+
+  def total_sugar
+    pfc_items.sum(:adjusted_sugar)
+  end
+
+  def total_oil
+    pfc_items.sum(:adjusted_oil)
+  end
 end
